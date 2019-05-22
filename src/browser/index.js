176cd6e61155dce43b5284452535fcf7a4231bce
TCIG-Hari <hari@mailcohen.com>
2019-05-22 18:03:27 +0300

    adding

---
+++ src/browser/index.js
@@ -0,0 +1,6 @@
+import react from "react";
+
+import {render} from"react-dom";
+import { App } from "../shared/App";
+
+render(<App/>, document.getElementById("root"));
---
+++ src/shared/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+
+export class App extends Component {
+
+    render(){
+        return (
+            <div className="App">
+        <div className="App-header">
+          <h2>Welcome to React in the Server</h2>
+        </div>
+        <p className="App-intro">Hello react</p>
+      </div>
+        );
+    }
+}
